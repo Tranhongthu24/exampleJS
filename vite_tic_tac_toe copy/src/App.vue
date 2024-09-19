07571d17819248efbb46766e89a729a7b183f0a7
hongthutran <httran@dainls.com>
2024-09-19 08:25:13 +0700

    g

---
+++ vite_tic_tac_toe copy/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="all">
+    <!-- <FetchingData />
+    <Data/> -->
+    <!-- <TreeView/> -->
+     <FlightBooker/>
+  </div>
+</template>
+
+<script setup>
+import FetchingData from "./components/FetchingData.vue";
+import Data from "./components/Data.vue";
+import TreeView from "./components/TreeView.vue";
+import FlightBooker from "./components/FlightBooker.vue";
+</script>
+<style scoped>
+.all{
+  margin: 50px;
+}
+</style>
